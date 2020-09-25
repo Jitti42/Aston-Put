75f75477465f225919437401310941563c055745
Jitti42 <70994903+Jitti42@users.noreply.github.com>
2020-09-25 23:34:14 +0700

    Add coding
    Add coding

---
+++ Fryer/Assets/Script/Cirby.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class Cirby : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rigidbody2D;
+    
+    public Vector2 velocity;
+    public UIManager uiManager;
+    private bool died;
+    //public void click;
+    void Start()
+    {
+        
+        animator = GetComponent<Animator>();
+        rigidbody2D = GetComponent<Rigidbody2D> ();
+        died = false; 
+    }
+
+    /*void OnClickStart()
+    {
+
+    }*/
+
+    void OnTriggerEnter2D (Collider2D c)
+    {
+        if (died)
+            return;
+
+        uiManager.IncreaseScore();
+        
+    }
+
+    void Update()
+    {
+        
+        bool mouseDown = Input.GetMouseButtonDown(0);
+
+        if (mouseDown && false == died)
+        {
+            rigidbody2D.velocity = velocity;
+        }
+        
+        
+        
+    }
+
+   
+
+    void OnCollisionEnter2D (Collision2D c)
+    {
+        died = true;
+        Invoke ("Ondied", 0);
+    }
+
+    void Ondied()
+    {
+       
+        uiManager.ShowResult();
+    }
+}
---
+++ Fryer/Assets/Script/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject hudPanel;
+    public GameObject resultPanel;
+    //HUD
+    public Text scoreText;
+    private int score;
+
+    //Result
+    public Text currentScoreText;
+    public Text bestScoreText;
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+
+    public void ShowResult()
+    {
+        hudPanel.SetActive(false);
+        resultPanel.SetActive(true);
+        currentScoreText.text = score.ToString();
+
+        int bestScore = PlayerPrefs.GetInt("BestScore");
+        bestScoreText.text = bestScore.ToString();
+        if(score>bestScore)
+        {
+            PlayerPrefs.SetInt("BestScore", score);
+            
+        }
+    }
+    public void OnReplayClick()
+    {
+        int level = Application.loadedLevel;
+        Application.LoadLevel(level);
+    }
+}
